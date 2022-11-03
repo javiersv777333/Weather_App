b7a4555e00e26f28b03014d6bccbf8e034e081c3
Carlos Javier Saez <antipasfiel@gmail.com>
2022-11-02 23:01:06 -0400

    Create application class.

---
+++ app/src/main/java/com/musalatask/weatherapp/application/WeatherApp.kt
@@ -0,0 +1,10 @@
+package com.musalatask.weatherapp.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApp : Application() {
+
+
+}
