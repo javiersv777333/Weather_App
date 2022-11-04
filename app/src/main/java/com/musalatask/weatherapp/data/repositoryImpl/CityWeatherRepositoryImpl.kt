47bc7faafaf5146962a4db14ecfc4a7fbe804a3b
Carlos Javier Saez <antipasfiel@gmail.com>
2022-11-03 23:04:19 -0400

    Implement the city weather repository

---
+++ app/src/main/java/com/musalatask/weatherapp/data/repositoryImpl/CityWeatherRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.musalatask.weatherapp.data.repositoryImpl
+
+import com.musalatask.weatherapp.common.Resource
+import com.musalatask.weatherapp.data.local.CityWeatherLocalDataSource
+import com.musalatask.weatherapp.data.model.CityWeather
+import com.musalatask.weatherapp.data.remote.CityWeatherRemoteDataSource
+import com.musalatask.weatherapp.data.utils.getNetworkBoundResource
+import com.musalatask.weatherapp.domain.repository.CityWeatherRepository
+import com.musalatask.weatherapp.domain.repository.GeocodingRepository
+import kotlinx.coroutines.flow.*
+import org.joda.time.DateTime
+import javax.inject.Inject
+
+class CityWeatherRepositoryImpl @Inject constructor(
+    private val localDataSource: CityWeatherLocalDataSource,
+    private val remoteDataSource: CityWeatherRemoteDataSource,
+    private val geocodingRepository: GeocodingRepository
+) : CityWeatherRepository {
+
+    override fun getCityWeather(cityName: String): Flow<Resource<CityWeather?>> =
+        geocodingRepository.getCoordinatesOfACity(cityName)
+            .flatMapConcat { coordinates ->
+                when (coordinates) {
+                    is Resource.Error -> flow { emit(Resource.Error(coordinates.message!!)) }
+                    is Resource.Success -> if (coordinates.data == null) flow {
+                        emit(Resource.Success(null))
+                    } else getCityWeather(
+                        cityName = cityName,
+                        latitude = coordinates.data!!.latitude,
+                        longitude = coordinates.data!!.longitude
+                    )
+                    else -> flow { emit(Resource.Loading()) }
+                }
+            }
+
+    private fun getCityWeather(
+        cityName: String,
+        latitude: Double,
+        longitude: Double
+    ): Flow<Resource<CityWeather?>> =
+        getNetworkBoundResource(
+            query = { localDataSource.getCityWeather(cityName) },
+            fetch = { remoteDataSource.getCityWeather(latitude = latitude, longitude = longitude) },
+            saveFetchResult = { saveLocalACityWeather(it) }
+        )
+
+    private suspend fun saveLocalACityWeather(cityWeather: CityWeather) {
+        val now = DateTime.now().millis
+        cityWeather.lastUpdated = now
+        localDataSource.withTransaction {
+            localDataSource.insertCityWeather(cityWeather)
+        }
+    }
+}
--- app/src/main/java/com/musalatask/weatherapp/domain/repository/CityWeatherRepository.kt
+++ app/src/main/java/com/musalatask/weatherapp/domain/repository/CityWeatherRepository.kt
@@ -1,9 +1,10 @@
 package com.musalatask.weatherapp.domain.repository
 
+import com.musalatask.weatherapp.common.Resource
 import com.musalatask.weatherapp.data.model.CityWeather
 import kotlinx.coroutines.flow.Flow
 
 interface CityWeatherRepository {
 
-    fun getCityWeather(cityName: String): Flow<CityWeather>
+    fun getCityWeather(cityName: String): Flow<Resource<CityWeather?>>
 }
