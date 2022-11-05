78c5e1d75454526b8db1df37972e430f67a92234
Carlos Javier Saez <antipasfiel@gmail.com>
2022-11-05 03:46:24 -0400

    Refactor get network bound resource.

--- app/src/main/java/com/musalatask/weatherapp/data/utils/NetworkBoundResource.kt
+++ app/src/main/java/com/musalatask/weatherapp/data/utils/NetworkBoundResource.kt
@@ -5,36 +5,64 @@ import kotlinx.coroutines.flow.*
 import retrofit2.HttpException
 import java.io.IOException
 
-fun <T : Any> getNetworkBoundResource(
+fun <T : Any> getNetworkBoundWeatherResource(
     query: () -> Flow<T?>,
     fetch: suspend () -> T?,
-    saveFetchResult: suspend (T) -> Unit,
-    shouldFetch: (T?) -> Boolean = { true }
+    saveFetchResult: suspend (T) -> Unit
 ): Flow<Resource<T?>> =
-    merge(
-        query().map { Resource.Success(it) },
-        flow {
-            val data = query().first()
-            if (shouldFetch(data)) {
-                emit(Resource.Loading(data))
+    flow {
 
-                try {
-                    val result = fetch()
-                } catch (e: HttpException) {
-                    emit(
-                        Resource.Error(
-                            data = data,
-                            message = "Oops, something went wrong!"
-                        )
-                    )
-                } catch (e: IOException) {
-                    emit(
-                        Resource.Error(
-                            data = data,
-                            message = "Couldn't reach server, check your internet connection."
-                        )
-                    )
+        emit(Resource.Loading())
+
+        val data = query().first()
+        if (data != null) emit(Resource.Loading(data))
+        try {
+            val result = fetch()
+            if (result == null) emit(Resource.Error("Resource not found!"))
+            else {
+                saveFetchResult(result)
+                emit(Resource.Success(query().first()))
+            }
+        } catch (e: HttpException) {
+            emit(
+                Resource.Error(
+                    data = data,
+                    message = "Oops, something went wrong!"
+                )
+            )
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    data = data,
+                    message = "Couldn't reach server, check your internet connection."
+                )
+            )
+        }
+    }
+
+fun <T : Any> getNetworkBoundCoordinatesResource(
+    query: () -> Flow<T?>,
+    fetch: suspend () -> T?,
+    saveFetchResult: suspend (T) -> Unit
+): Flow<Resource<T?>> =
+    flow {
+
+        emit(Resource.Loading())
+
+        val data = query().first()
+        if (data != null) emit(Resource.Success(data))
+        else {
+            try {
+                val result = fetch()
+                if (result == null) emit(Resource.Error("Resource not found!"))
+                else {
+                    saveFetchResult(result)
+                    emit(Resource.Success(query().first()))
                 }
+            } catch (e: HttpException) {
+                emit(Resource.Error("Oops, something went wrong!"))
+            } catch (e: IOException) {
+                emit(Resource.Error("Couldn't reach server, check your internet connection."))
             }
         }
-    )
+    }
