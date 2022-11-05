dd09834aa38fc6cd3de4c068d810c6e64a51e0c3
Carlos Javier Saez <antipasfiel@gmail.com>
2022-11-05 03:53:57 -0400

    Fix bug.

--- app/src/main/java/com/musalatask/weatherapp/data/local/CityWeatherLocalDataSource.kt
+++ app/src/main/java/com/musalatask/weatherapp/data/local/CityWeatherLocalDataSource.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface CityWeatherLocalDataSource {
 
-    fun getCityWeather(cityName: String): Flow<CityWeather>
+    fun getCityWeather(cityName: String): Flow<CityWeather?>
     suspend fun insertCityWeather(cityWeather: CityWeather)
     suspend fun <R> withTransaction(block: suspend () -> R): R
 }
